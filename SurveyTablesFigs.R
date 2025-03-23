2032df0cbc79206147f1cd244b94f57e9a4a93ce
KCMeyer <Kathryn.Meyer@dfw.wa.gov>
2025-03-23 07:15:19 -0700

    moving files to JAC repository

---
+++ SurveyTablesFigs.R
@@ -0,0 +1,54 @@
+# General notes - choose color-blind friendly palette, inlcude number of response stats for each question
+
+# tabulate the number of people by geo area relative to census data - how far off the mark?
+
+
+#Tables and Figures To-Do List:
+
+### 1.) Summary of responses/what's presented: Include table w/residency status. Total # submissions (include completes and partials), removed non-resident/visitor (include #) surveys for this purpose 
+### 2.) Make histogram of duplicate IP's, state the ways we limited the survey responses (one per device and CAPTCHA) 
+### 3.) In which area do you live Q
+          # - re-do as bar plot w/ table
+          # - include table of residents by location and add column with 2020 census data by location
+          # - reassign 'other' category (delete non-Jeffco, keep unassigned as 'other')
+### 4.) What is your age?
+        # - re-do as bar plot (percentage - scale to 100)
+        # - table of column sums across age groups, county-wide - include data on age distribution from census data
+        # require some manual review/edits - adjust non-numeric entries
+### 5.) how important is it to you to have an aquatic facility?
+        # - barplot (percentages) w/table (numbers and percentages)
+### 6.) swimming ability question
+      # - sum across each age group and swimming ability category
+      # - create bar chart w/ percentages within each age category
+      # - include national data on swimming ability?
+### 7.) have you used an aquatic facility in last 2 years?
+      # - keep as pie chart
+### 8.) which ones did you use?
+      # - ordered bar chart (high to low) as percentages
+      # - include list of the 'other'
+### 9.) how often do you use an aquatic facility?
+      # - keep as pie chart w/custom color palette and percentages on chart
+### 10.) aquatic amenities
+      # - keep as bar chart, reverse order overall
+      # - produce age-based figures as well, may not use in presentation
+      # - select top 5 per age group
+      # - work on a way to bin household ages (mean household age) - if we have time??
+### 11.) Non aquatic - exactly the same as above
+### 12.) How much more would you use an aquatic facility, if amenities?
+      # include pie chart 
+      # also include chart with comparison to current useage (bar chart with two colors to show before and after)
+### 13.) non-aquatic amenities
+      # include pie chart
+      # include before and after chart as well?
+      # what the survey does and doesn't show here
+### 14.) tax Q
+      # include as pie-chart w/percentages (raw, unweighted)
+      # bar chart by geographic area 
+      # bar chart by person responding to represent voting status?
+      # weighted pie-chart overall??
+
+### are there other categories that matter and/or other ways to weight the data?
+
+
+
+
